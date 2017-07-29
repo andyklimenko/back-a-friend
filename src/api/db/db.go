703ba72843bc40ca6d75a2b1ab3ded11c1057b91
andyklimenko <andrey.klimenko@gmail.com>
2017-07-29 15:29:32 +0300

    Creating tournament

--- src/api/db/db.go
+++ src/api/db/db.go
@@ -4,18 +4,27 @@ import (
 	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
+	"errors"
 )
 
 const (
-	createTournamentsTable = "CREATE TABLE 'Tournaments' ( `TourId` INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, `Winners` TEXT NOT NULL )"
-	createWinnersTable = "CREATE TABLE `Winners` ( `Winner-id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `Player-id` INTEGER NOT NULL, `Prize` INTEGER NOT NULL )"
+	createTournamentsTable = "CREATE TABLE 'Tournaments' ( `TourId`	INTEGER NOT NULL UNIQUE, `Winners` TEXT, `Deposit` INTEGER NOT NULL, `Players` TEXT, PRIMARY KEY(TourId))"
+	createWinnersTable = "CREATE TABLE 'Winners' (`Winner-id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `TourId` INTEGER NOT NULL, `Player-id`	TEXT NOT NULL, `Prize` INTEGER NOT NULL)"
+
+	announceTournamentQuery = "insert into Tournaments values (?, '', '', ?)"
+	selectTournamentPlayersQuery = "select Players from Tournaments where TourId=?"
+)
+
+var (
+	ErrorNotFound = errors.New("Not found")
+	ErrAlreadyExists = errors.New("Already exists")
 )
 
 type Db struct {
 	db *sql.DB
 }
 
-func (d Db) Create(dbPath string) error {
+func (d *Db) Create(dbPath string) error {
 	var err error
 	d.db, err = sql.Open("sqlite3", dbPath)
 	if err != nil {
@@ -38,6 +47,57 @@ func (d Db) Create(dbPath string) error {
 	if _, err = statement.Exec(); err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func (d *Db) CreateTournament(id int, deposit int) error {
+	tx, err := d.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare(announceTournamentQuery)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(id, deposit)
+	if err != nil {
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *Db) JoinTournament(tourId int, playerId string, backers []string) (rerr error) {
+	tx, err := d.db.Begin()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if rerr != nil {
+			tx.Rollback()
+		}
+	}()
+
+	rows, err := tx.Query(selectTournamentPlayersQuery, tourId)
+	if err != nil {
+		return err
+	}
+
+	var players string
+	if err := rows.Scan(&players); err != nil {
+		return err
+	}
+
+	// todo: parse players
 
 	return nil
 }
--- src/api/db/db_test.go
+++ src/api/db/db_test.go
@@ -7,22 +7,76 @@ import (
 	"path"
 )
 
-func TestDb_Create(t *testing.T) {
-	var mydb Db
+func setupMyDb() (_ *Db, _ func(), rerr error) {
+	mydb := &Db{}
 
 	tmpDir, err := ioutil.TempDir("", "dbTest")
 	if err != nil {
-		t.Fatal(err)
+		return nil, func(){}, err
 	}
-	defer os.Remove(tmpDir)
+	defer func() {
+		if rerr != nil {
+			os.Remove(tmpDir)
+		}
+	}()
 
 	dbDir := path.Join(tmpDir, "db")
 	if err := os.MkdirAll(dbDir, 0777); err != nil {
-		t.Fatal(err)
+		return nil, func(){}, err
 	}
 
 	dbPath := path.Join(dbDir, "testdb.db")
 	if err := mydb.Create(dbPath); err != nil {
-		t.Error(err)
+		return nil, func(){}, err
+	}
+
+	closer := func() {
+		mydb.db.Close()
+		os.Remove(tmpDir)
+	}
+
+	return mydb, closer, nil
+}
+
+func TestDb_CreateTournament(t *testing.T) {
+	myDb, closer, err := setupMyDb()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer closer()
+
+	const tournamentId = 100500
+	const deposit = 1000
+	if err := myDb.CreateTournament(tournamentId, deposit); err != nil {
+		t.Fatal(err)
+	}
+
+	rows, err := myDb.db.Query("select * from Tournaments where TourId=?", tournamentId)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var tourId, depo int
+	var winners, players string
+
+	rows.Next()
+	if err := rows.Scan(&tourId, &winners, &players, &depo); err != nil {
+		t.Fatal(err)
+	}
+
+	if tourId != tournamentId {
+		t.Error(tourId)
+	}
+
+	if winners != "" {
+		t.Error(winners)
+	}
+
+	if players != "" {
+		t.Error(players)
+	}
+
+	if depo != deposit {
+		t.Error(depo)
 	}
 }
